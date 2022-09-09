ce286ed0b7216ea564b8ba8fea683d45b7146a0b
Aman Sharma <55488429+aman-coder@users.noreply.github.com>
2022-09-09 12:22:18 +0530

    Create debug_string_retrun_char.java

---
+++ debug_string_retrun_char.java
@@ -0,0 +1,37 @@
+#### Nth character in Decrypted String 
+import java.util.*;
+public class Main
+{
+    static String charo(String aa, int n){
+        ArrayList<Character> arr = new ArrayList<Character>();
+        char temp='\0';
+        for(int i=0;i<aa.length();i++){
+            if(aa.charAt(i)>='a' && aa.charAt(i)<='z'){
+                arr.add(aa.charAt(i));
+                temp = aa.charAt(i);
+            }
+            if(aa.charAt(i)>='1' && aa.charAt(i)<='9'){
+                for(int j=1;j<Integer.parseInt(String.valueOf(aa.charAt(i)));j++)
+                {
+                arr.add(temp);
+                }
+            }
+        }
+        if(arr.size()<n){
+            return "-1";
+        }else{
+             return String.valueOf(arr.get(n-1));
+            //return arr;
+        }
+    }
+	public static void main(String[] args) {
+	    Scanner sc = new Scanner(System.in);
+	    String aa;
+	    int n;
+		System.out.println("Enter the String");
+		aa=sc.nextLine();
+		System.out.println("Enter the number of Character");
+		n=sc.nextInt();
+		System.out.println("The Requested Character is: "+charo(aa,n));
+	}
+}
